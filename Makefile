61a4ef6acca088f3ffb1356294e1c58d29e5725c
mrcconaway <mrcconaway@gmail.com>
2022-07-20 15:41:27 -0400

    Figured out why drawWireFrameModel no work. It already centers itself to the player in the function

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 a.out:
 	g++ -o tds *.cpp -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17
 	./tds
-a.exe:
+w:
 	g++ -o tds *.cpp -luser32 -lgdi32 -lopengl32 -lgdiplus -lShlwapi -ldwmapi -lstdc++fs -static -std=c++17
+	./tds
--- game.cpp
+++ game.cpp
@@ -17,11 +17,11 @@ bool game::OnUserCreate()
 			  defhp,                                  // hp
 			  defdmg};                                // dmg 
 
-	// vecModelPlayer = {{player.x + 5, player.y - 5},
-	// 				  {player.x - 5, player.y - 5},
-	// 				//   {player.x + 5, player.y + 5},
-	// 				//   {player.x - 5, player.y + 5}
-	// 				 };
+	vecModelPlayer = {{+5.0f,  -5.0f},
+					  {-5.0f,  -5.0f},
+					//   {player.x + 5, player.y + 5},
+					//   {player.x - 5, player.y + 5}
+					 };
 
 
 	return true;
@@ -81,18 +81,16 @@ void game::drawCrosshair()
 
 void game::drawPlayer()
 {
-	for(int x = player.x - 5; x < player.x+5; ++x){
-		for(int y = player.y - 5; y < player.y+5; ++y){
-      		PixelGameEngine::Draw(x, y, olc::Pixel(0,0,0));
-		}
-	}
-	// DrawWireFrameModel(vecModelPlayer, player.x, player.y, player.angle);
+	// for(int x = player.x - 5; x < player.x+5; ++x){
+	// 	for(int y = player.y - 5; y < player.y+5; ++y){
+    //   		PixelGameEngine::Draw(x, y, olc::Pixel(0,0,0));
+	// 	}
+	// }
+	DrawWireFrameModel(vecModelPlayer, player.x, player.y, player.angle);
 }
 
 void game::DrawWireFrameModel(const std::vector<std::pair<float,float>> &vecModelCoordinates, float x, float y, float r , float s, const olc::Pixel)
 {
-
-	#include <iostream>
 	// pair.first = x coordinate
 	// pari.second = y coordinate
 
@@ -118,9 +116,6 @@ void game::DrawWireFrameModel(const std::vector<std::pair<float,float>> &vecMode
 	}
 	// Draw Closed Polygon
 	for(int i = 0; i < verts; i++){
-		std::cout << verts << std::endl;
-		std:: cout << vecTransformedCoordinates[i % verts].first << " " << vecTransformedCoordinates[i % verts].second << std::endl;
-		std::cout << ScreenWidth() << " " << ScreenHeight() << std::endl; 
 		int j = (i+1);
 		DrawLine(vecTransformedCoordinates[i % verts].first, vecTransformedCoordinates[i % verts].second, 
 				vecTransformedCoordinates[j % verts].first, vecTransformedCoordinates[j % verts].second);
